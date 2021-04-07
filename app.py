c1762b19b937cd714aa1c8395acfd8646670b837
kwinkunks <matt@agilegeoscience.com>
2021-04-07 09:18:48 -0300

    catch up to deployed version

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, make_response
 
 import utils
 
@@ -35,18 +35,26 @@ def plot():
     else:
         log = True
 
-    drop = request.args.get('drop') or 'false'
-    if drop.lower() in ['0', 'false', 'off', 'no']:
-        drop = False
-    else:
+    drop = request.args.get('drop') or 'true'
+    if drop.lower() in ['1', 'true', 'on', 'yes']:
         drop = True
+    else:
+        drop = False
 
     years = utils.get_years()
     G = utils.get_network(years)
     if len(G) < 1:
         return render_template('plot.html', result={})
     result = {'network_plot': utils.plot_network(G, years, scale=scale)}
-    result['years_plot'] = utils.plot_years(years, drop=drop, log=log)
+
+    result['years_plot'] = utils.plot_bars(years, sort=True, drop=drop, log=log)
+
+    lasts = utils.get_lasts()
+    result['lasts_plot'] = utils.plot_bars(lasts, title="Current position")
+
+    lens = utils.get_lens()
+    result['lens_plot'] = utils.plot_bars(lens, title="Career length so far", lpos=0.5)
+
     return render_template('plot.html', result=result)
 
 
@@ -58,13 +66,27 @@ def about():
     return render_template('about.html')
 
 
+@app.route('/data', methods=['GET'])
+def data():
+    """
+    Serve the log file.
+    """
+    with open('log.txt') as f:
+        data = f.read()
+    response = make_response(data, 200)
+    response.mimetype = "text/plain"
+    return response
+
+
 @app.route('/delete', methods=['DELETE'])
 def delete():
     """
     Delete the database.
     """
     if request.method == 'DELETE':
-        # _ = os.remove("log.txt")
+        # _ = os.remove("log.txt")  # Keep this
         _ = os.remove("edges.db")
         _ = os.remove("nodes.db")
+        _ = os.remove("lasts.db")
+        _ = os.remove("lens.db")
         return "Done"
--- utils.py
+++ utils.py
@@ -6,6 +6,8 @@ import base64
 from io import BytesIO
 import matplotlib.pyplot as plt
 import numpy as np
+from collections import OrderedDict
+from collections import defaultdict
 
 
 VOCAB = ["undergrad", "postgrad", "faculty", "academic",
@@ -19,6 +21,7 @@ VOCAB = ["undergrad", "postgrad", "faculty", "academic",
          "other", 'break'
         ]
 
+
 def get_info(record):
     """
     Take a single response and turn it into a list of careers.
@@ -27,7 +30,7 @@ def get_info(record):
     """
     items = [tuple(i.strip().split()) for i in record.split(',')]
     items = filter(None, items)
-    path, years = [], {}
+    path, years = [], defaultdict(int)
     for pair in items:
         # Get employment.
         m = difflib.get_close_matches(pair[0], VOCAB, n=1, cutoff=0.5)
@@ -44,7 +47,7 @@ def get_info(record):
             y = 1
         except IndexError:  # None provided
             y = 1
-        years[job] = y
+        years[job] += y
 
     return path, years
 
@@ -60,6 +63,12 @@ def store(record):
         for k, v in years.items():
             vi = db.get(k, 0)
             db[k] = vi + v
+    with shelve.open('lasts') as db:
+        last = path[-1]
+        db[last] = db.get(last, 0) + 1
+    with shelve.open('lens') as db:
+        length = str(int(sum(years.values())))
+        db[length] = db.get(length, 0) + 1
     return 'Thank you!'
 
 
@@ -88,32 +97,55 @@ def get_network(years):
 def get_years():
     with shelve.open('nodes') as db:
         d = dict(db).copy()
-
     return d
+
+
+def get_lasts():
+    with shelve.open('lasts') as db:
+        d = dict(db)
+    return d
+
+
+def chunks(lst, n):
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
+
+
+def get_lens():
+    with shelve.open('lens') as db:
+        d = dict(db)
+
+    bins = list(chunks([str(n) for n in range(50)], 5))
+    labels = ['<5', '5-9', '10-14', '15-19', '20-24', '25-29',
+              '30-34', '35-39', '40-44', '45-49', '>50']
+    data = [0 for l in labels]
+    for k, v in d.items():
+        for idx, row in enumerate(bins):
+            if k in row: break
+        data[idx] += v
+
+    return OrderedDict((l, d) for l, d in zip(labels, data))
 
 
 def plot_network(G, years, scale=10):
     """
     Make a networkx plot and convert to base64-encoded string.
     """
-
-    print(nx.__version__)
-
     edges = G.edges()
     weights = [G[u][v]['weight'] for u,v in edges]
 
-    print(nx.get_node_attributes(G, 'count'))
     counts = [scale * nx.get_node_attributes(G, 'count')[u] for u in G.nodes()]
 
     params = {
         'node_size': counts,
         'with_labels': True,
+        'verticalalignment': 'bottom',
         'width': weights,
     }
 
     pos = nx.spring_layout(G)
 
-    fig = plt.figure(figsize=(8, 8))
+    fig = plt.figure(figsize=(12, 12))
     nx.draw(G, pos, **params)
 
     # Save as base64 string.
@@ -126,7 +158,8 @@ def plot_network(G, years, scale=10):
     return figdata_png.decode('utf8')
 
 
-def plot_years(data, drop=False, log=False):
+def plot_bars(data, drop=False, sort=False, log=False, title=True, lpos=None):
+
 
     if drop:
         _ = data.pop('undergrad', None)
@@ -137,15 +170,15 @@ def plot_years(data, drop=False, log=False):
     labels = list(data.keys())
     values = list(data.values())
 
-    values = sorted(values, reverse=True)
-    labels = sorted(labels, key=lambda li: values[labels.index(li)], reverse=True)
+    if sort:
+        labels = [l for _, l in sorted(zip(values, labels), reverse=True)]
+        values = sorted(values, reverse=True)
 
     y = list(range(len(values)))
     y_min, y_max = y[0]-0.75, y[-1]+0.75
 
     fig, ax = plt.subplots(figsize=(8, 8))
     bars = ax.barh(y, values, color='orange', align='center', edgecolor='none')
-    bars[np.argmax(values)].set_color('red')
     ax.set_yticks(y)
     if log:
         ax.set_xscale('log')
@@ -154,10 +187,19 @@ def plot_years(data, drop=False, log=False):
     ax.grid(c='black', alpha=0.15, which='both')
     ax.patch.set_facecolor("white")
     fig.patch.set_facecolor("none")
-    ax.set_title("{:.2f} person-careers of experience".format(sum(values)/40))
 
+    if title is True:
+        t = "{:.2f} person-careers of experience".format(sum(values)/40)
+    elif title:
+        t = title
+    else:
+        t = ""
+    ax.set_title(t)
+
+    if lpos is None:
+        lpos = min(values)
     for i, d in enumerate(values):
-        ax.text(1, i, "{}".format(int(d)), va='center', size=12)
+        ax.text(lpos, i, "{}".format(int(d)), va='center', size=12)
 
     plt.tight_layout()
 
@@ -171,4 +213,3 @@ def plot_years(data, drop=False, log=False):
     figdata_png = base64.b64encode(handle.getvalue()).decode('utf8')
 
     return figdata_png
-
